547358f25e4910d7f75522779dc9a291f95e77ab
mcmah309 <dillonmcmahon79@gmail.com>
2025-03-12 15:39:06 +0000

    Initial desktop support

--- dioxus_file_picker/src/lib.rs
+++ dioxus_file_picker/src/lib.rs
@@ -1,6 +1,25 @@
-use std::{collections::HashSet, path::PathBuf, sync::Arc};
+use std::{
+    collections::HashSet,
+    env,
+    path::PathBuf,
+    sync::Arc,
+    time::{self},
+};
 
-use dioxus::{html::FileEngine, logger::tracing::error, prelude::*};
+use dioxus::{
+    html::FileEngine,
+    logger::tracing::{error, warn},
+    prelude::*,
+};
+#[cfg(any(
+    target_os = "windows",
+    target_os = "macos",
+    target_os = "linux",
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd"
+))]
 use rfd::FileDialog;
 #[cfg(target_arch = "wasm32")]
 use web_sys::{Blob, File};
@@ -21,6 +40,8 @@ pub fn FilePickerLauncher(
     /// File extensions to accept
     // accept: Vec<String>, // todo
     on_submit: Callback<(Arc<dyn FileEngine>, HashSet<PathBuf>), ()>,
+    /// The path to open the directory at. If null, defaults to current directory. Has no effect on web.
+    open_directory_path: Option<PathBuf>,
     children: Element,
 ) -> Element {
     #[cfg(target_arch = "wasm32")]
@@ -61,56 +82,88 @@ pub fn FilePickerLauncher(
             }
         }
     }
-    #[cfg(not(target_arch = "wasm32"))]
+    #[cfg(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "linux",
+        target_os = "dragonfly",
+        target_os = "freebsd",
+        target_os = "netbsd",
+        target_os = "openbsd"
+    ))]
     {
-        #[cfg(any(
-            target_os = "windows",
-            target_os = "macos",
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd"
-        ))]
-        {
-            if desktop_native {
-                // todo launch native window
-
-                return rsx! {
-                    div {
-                        onclick: |_| {
-                            let _ = FileDialog::new().set_directory("/").pick_file();
-                        },
-                        {children}
-                    }
-                };
-            } else {
-                return rsx! {
-                    div {
-                        onclick: move |_| {
-                            let rsx = rsx! {
-                                cross_platform::CrossPlatformFilePicker { multiple, on_submit } // todo bootstrap the on_submit to go back
-                            };
-                            // if desktop_windowed {
-                            //     let dom = VirtualDom::new(rsx);
-                            //     dioxus::desktop::window().new_window(dom, Default::default());
-                            // } else {
-                            //     let nav = use_navigator();
-                            //     nav.push(rsx);
-                            // }
-                        },
-                        {children}
-                    }
-                };
+        let launch_cross_platform_file_picker = move |_| {
+            if desktop_native || desktop_windowed {
+                let dom = VirtualDom::new_with_props(
+                    cross_platform::CrossPlatformFilePicker,
+                    cross_platform::CrossPlatformFilePickerProps {
+                        multiple,
+                        on_submit,
+                    },
+                );
+                dioxus::desktop::window().new_window(dom, Default::default());
             }
-        }
-        rsx! {
-            div {
-                onclick: move |_| {
-                    let nav = use_navigator(); // todo use with back navigation
-                    // nav.push(cross_platform::CrossPlatformFilePicker { multiple, on_submit });
+            let rsx = rsx! {
+                cross_platform::CrossPlatformFilePicker { multiple, on_submit } // todo bootstrap the on_submit to go back
+            };
+            Vec::new()
+        };
+        if desktop_native {
+            let on_click = move |event| {
+                let path_clone = open_directory_path.clone();
+                let path = path_clone.unwrap_or_else(|| {
+                    env::current_dir().expect("Failed to get current directory")
+                });
+                let start_time = time::Instant::now();
+                let mut files;
+                if multiple {
+                    files = rfd::FileDialog::new()
+                        .set_directory(&path)
+                        .pick_files()
+                        .unwrap_or(Vec::new())
+                } else {
+                    files = rfd::FileDialog::new()
+                        .set_directory(&path)
+                        .pick_file()
+                        .map(|e| vec![e])
+                        .unwrap_or(Vec::new());
                 }
-            }
+                let elapsed = start_time.elapsed();
+                let within_one_second = elapsed <= time::Duration::from_secs(1);
+                if within_one_second {
+                    debug_assert!(files.is_empty());
+                    warn!(
+                        "Native file dialog closed too quickly. This was likely an error. Launching a dioxus file dialog instead"
+                    );
+                    files = launch_cross_platform_file_picker(event);
+                }
+                // todo
+            };
+            return rsx! {
+                input {
+                    name: "textreader",
+                    r#type: "file",
+                    multiple,
+                    accept: ".txt,.rs",
+                }
+                div { onclick: on_click, {children} }
+            };
+        } else {
+            return rsx! {
+                div {
+                    onclick: move |event| {
+                        let files = launch_cross_platform_file_picker(event);
+                    },
+                    {children}
+                }
+            };
+        }
+    }
+    rsx! {
+        div {
+            onclick: move |_| {
+                let nav = use_navigator();
+            },
         }
     }
 }
